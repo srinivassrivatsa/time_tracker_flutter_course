35d97ca34ed8f6816ddd5eb82b108f002c586212
K T Srinivas Srivatsa <44376549+srinivassrivatsa@users.noreply.github.com>
2021-05-13 07:37:41 +0530

    refactored home page body into a private method

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -11,17 +11,38 @@ class SignInPage extends StatelessWidget {
         ),
         elevation: 2.0,
       ),
-      body: Container(
-        color: Colors.yellow,
-        child: Column(
-          children: <Widget>[Container(
+      body: _buildContent(),
+    );
+  }
+
+  Widget _buildContent() {
+    return Container(
+      color: Colors.yellow,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
             color: Colors.orange,
             child: SizedBox(
               height: 100.0,
               width: 100.0,
             ),
-          )],
-        ),
+          ),
+          Container(
+            color: Colors.red,
+            child: SizedBox(
+              height: 100.0,
+              width: 100.0,
+            ),
+          ),
+          Container(
+            color: Colors.purple,
+            child: SizedBox(
+              height: 100.0,
+              width: 100.0,
+            ),
+          ),
+        ],
       ),
     );
   }
