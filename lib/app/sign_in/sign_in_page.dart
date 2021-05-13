99e8d399309de470b70eb7473129bbc7aae9ba2e
K T Srinivas Srivatsa <44376549+srinivassrivatsa@users.noreply.github.com>
2021-05-13 08:29:34 +0530

    Added elevated button to sign in with google

--- lib/app/sign_in/sign_in_page.dart
+++ lib/app/sign_in/sign_in_page.dart
@@ -30,23 +30,7 @@ class SignInPage extends StatelessWidget {
           SizedBox(
             height: 8.0,
           ),
-          Container(
-            color: Colors.red,
-            child: SizedBox(
-              height: 100.0,
-              width: 100.0,
-            ),
-          ),
-          SizedBox(
-            height: 8.0,
-          ),
-          Container(
-            color: Colors.purple,
-            child: SizedBox(
-              height: 100.0,
-              width: 100.0,
-            ),
-          ),
+          ElevatedButton(child: Text('Sign in with Google'), onPressed: () {}),
         ],
       ),
     );
